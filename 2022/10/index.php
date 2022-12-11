13ee1f5daef7fd2d159c6621980adc70c37c1d9d
Muhammed Ali Alat <muhammedali@alat.de>
2022-12-11 08:15:26 +0100

    Day 11

--- 2022/10/index.php
+++ 2022/10/index.php
@@ -12,9 +12,9 @@ function getPart1($array) {
 	$x = 1;
 	$cycle = 1;
 	$sum = 0;
-
+	
 	foreach ($array as $line) {
-
+		
 		$command = explode(' ', $line);
 		if ($command[0] == 'noop') {
 			if (($cycle+20)%40 == 0) {
@@ -27,26 +27,26 @@ function getPart1($array) {
 			} elseif (($cycle+21)%40 == 0) {
 				$sum += (($cycle+1) * $x);
 			}
-
+			
 			$cycle += 2;
 			$x += (int) $command[1];
 		}
 	}
-
+	
 	return $sum;
 }
 
 function getPart2($array) {
 	$x = 1;
 	$cycle = 1;
-
+	
 	foreach ($array as $line) {
 		if (($cycle - 1 - $x) * ($cycle - 1 - $x) < 2) {
 			echo 'X';
 		} else {
 			echo '.';
 		}
-
+		
 		$command = explode(' ', $line);
 		if ($command[0] == 'noop') {
 			if ($cycle%40 == 0) {
@@ -69,7 +69,7 @@ function getPart2($array) {
 				$x += 40;
 				echo chr(10);
 			}
-
+			
 			$cycle += 2;
 			$x += (int) $command[1];
 		}
